58a4c68b5ba1aa1c3047f3078beb237ee68520d3
drea295 <david.rea295@gmail.com>
2024-04-15 10:30:13 +0100

    images

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 
 spaceship = Spaceship()
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
 
 while True:
     for event in pygame.event.get():
@@ -24,6 +26,7 @@ while True:
             sys.exit()
 
     screen.fill(grey)
+    spaceship_group.draw(screen)
     
     pygame.display.update()
     clock.tick(60)
