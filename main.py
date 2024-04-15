2eddcc8f0d30745d7d3c2969027b255335c4c339
drea295 <david.rea295@gmail.com>
2024-04-15 10:11:32 +0100

    initial pygame setup

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import pygame
+
+pygame.init()
+
+screen_width = 750
+screen_height = 700
+
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+
+pygame.display.set_caption("Space Invaders")
+
+clock = pygame.time.Clock(60)
