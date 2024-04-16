39d62738860cdaad1860efdc1d49dd31a0f42afa
drea295 <david.rea295@gmail.com>
2024-04-16 17:10:01 +0100

    update aliens

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import pygame
 from spaceship import Spaceship
 from obstacle import Obstacle, grid
+from alien import Alien
 
 class Game:
     def __init__(self, screen_width, screen_height):
@@ -10,6 +11,8 @@ class Game:
         self.spaceship_group = pygame.sprite.GroupSingle()
         self.spaceship_group.add(Spaceship(self.screen_width, self.screen_height))
         self.obstacles = self.create_obstacles()
+        self.aliens_group = pygame.sprite.Group()
+        self.create_aliens()
 
     def create_obstacles(self):
         obstacle_width = len(grid[0]) * 3
@@ -20,4 +23,20 @@ class Game:
             obstacle = Obstacle(offset_x, self.screen_height - 100)
             obstacles.append(obstacle)
         return obstacles
+    
+    def create_aliens(self):
+        for row in range(5):
+            for column in range(11):
+                x = 75 + column * 55
+                y = 110 + row * 55
+
+                if row == 0: 
+                    alien_type = 3
+                elif row in (1,2 ): 
+                    alien_type = 2
+                else:
+                    alien_type = 1
+
+                alien = Alien(alien_type, x, y)
+                self.aliens_group.add(alien)
         
