aad91992b0375ea951bc504fc3efe02c1a9ec124
drea295 <david.rea295@gmail.com>
2024-04-16 17:02:02 +0100

    add alien inital setup

---
+++ alien.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.sprite import _Group
+
+class Alien(pygame.sprite.Sprite):
+    def __init__(self, type, x, y):
+        super().__init__()
+        self.type = type
+        path = f"Graphics/alien_{type}.png"
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(topleft = (x, y))
